8dc788721f17b42dbc2ef7b33c0437fe349d93a0
kkitetsu <b787kitetsu@gmail.com>
2024-03-22 14:41:00 +0900

    Implement sql statement

--- src/main/java/com/example/todo/controller/WorkTimeManagementController.java
+++ src/main/java/com/example/todo/controller/WorkTimeManagementController.java
@@ -1,11 +1,17 @@
 package com.example.todo.controller;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
+
+import com.example.todo.entity.LogsEntity;
+import com.example.todo.service.EmployeesInfoService;
 
 import jakarta.servlet.http.HttpSession;
 
@@ -15,15 +21,23 @@ public class WorkTimeManagementController {
 	/**
 	 * @author kk
 	 * 
+	 * Employees information service.
+	 */
+	@Autowired
+	private EmployeesInfoService employeesInfoService;
+	
+	/**
+	 * @author kk
+	 * 
 	 * Access to user my page.
 	 * 
 	 * @return 
 	 */
-/*	@GetMapping(value="/userMyPage")
-	public String displayUserMyPage(Model model, HttpSession session) {
+	@GetMapping(value="/userMyPage")
+	public String UserMyPage(Model model, HttpSession session) {
 		return "/userMyPage";
 	}
-*/
+
 	@GetMapping(value="/home")
 	public String displayUserMyPage(Model model, HttpSession session) {
 		return "/home";
@@ -44,13 +58,21 @@ public class WorkTimeManagementController {
 															@RequestParam("selectedOption") String selectedOption) {
 		
 		if (action.equals("clockin")) {
+			LogsEntity logsEntity = new LogsEntity();
+			logsEntity.setApplicant("Honnin");
+			logsEntity.setNote("XXXX");
+			logsEntity.setUser_id(1);
+			logsEntity.setStampType_id(Integer.parseInt(selectedOption));
 			System.out.println(selectedOption);
+			employeesInfoService.updateLogs(logsEntity);
+			
             return "/alertAndRedirect";
+            
         } else if (action.equals("checkHistory")) {
-            return "/userLogPage";
+            return "redirect:/userLogPage";
+            
         } else {
-        	// TODO: redirect to top page
-            return "";
+            return "/home";
         }
 	}
 	
@@ -61,9 +83,11 @@ public class WorkTimeManagementController {
 	 * 
 	 * @return clockinPage
 	 */
-	@GetMapping("/tmppage")
-	public String tmpPage() {
-	    return "userLogPage"; 
+	@GetMapping("/userLogPage")
+	public String userLogPage(Model model, HttpSession session) {
+		List<LogsEntity> logs = employeesInfoService.getEmployeesLogs();
+		model.addAttribute("logs", logs);
+		return "userLogPage"; 
 	}
 	
 }
